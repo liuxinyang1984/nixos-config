58fbea2dad0136506c4db52186ac50cdeace9674
Mr.Cookie <liuxinyang1984@gmail.com>
2025-05-29 17:43:55 +0800

    修复目录错误

--- home-manager/users/cookie.nix
+++ home-manager/users/cookie.nix
@@ -11,6 +11,7 @@
   home.username = "cookie";
   home.homeDirectory = "/home/cookie";
 
+  home.stateVersion = "23.05";  # 必须设置，匹配你NixOS版本
   # 个人软件包
   home.packages = with pkgs; [
     # 开发工具
@@ -25,15 +26,6 @@
   
   # 自定义文件
   home.file = {
-    ".ssh/authorized_keys".source = ../../ssh/authorized_keys;
+    ".ssh/authorized_keys".source = ../ssh/authorized_keys;
   };
-
-  # 管理目录权限
-  home.directories = {
-    ".ssh" = {
-      recursive = true;
-      permissions = "0700";
-    };
-  };
-
 }
