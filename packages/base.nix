6094ad655deb9e01c9032c2f685f39289fbdf7c2
installer <installer@gmail.com>
2025-06-03 10:14:00 +0800

    添加iwctl

--- packages/base.nix
+++ packages/base.nix
@@ -14,8 +14,15 @@
     xorg.xinput
     busybox
     pulsemixer
+    iwd
   ];
 
+# 在 NixOS 配置中启用 IWD 服务
+# networking.wireless.enable = true;
+# networking.iwd.enable = true;
+
+networking.wireless.iwd.enable = true;  # 启用 iwd
+networking.networkmanager.enable = false; # 确保 NetworkManager 已禁用（若存在）
   # 其它软件引用
   imports = [
     ../modules/ssh.nix
